7ef7ae23dc61633b796bc04779cda02f3a5c9441
Francisco Pasquier <franciscopasquier@pop-os.localdomain>
2023-09-12 12:37:30 -0300

    check collisions

--- test/test_models.py
+++ test/test_models.py
@@ -7,21 +7,21 @@ class TestBagTiles(unittest.TestCase):
     @patch('random.shuffle')
     def test_bag_tiles(self,patch_shuffle):
         bag = BagTiles()
-        self.assertEqual(len(bag.tiles),100)
+        self.assertEqual(len(bag.tiles),102)
         self.assertEqual(patch_shuffle.call_count,1)
         self.assertEqual(patch_shuffle.call_args[0][0],bag.tiles)
 
     def test_take(self):
         bag = BagTiles()
         tiles = bag.take(2)
-        self.assertEqual(len(bag.tiles), 98)
+        self.assertEqual(len(bag.tiles), 100)
 
     def test_put(self):
         bag = BagTiles()
         put_tiles = [Tile('Z', 10), Tile('Y', 8)]
         bag.put(put_tiles)
-        self.assertEqual(len(bag.tiles), 102)
+        self.assertEqual(len(bag.tiles), 104)
 
     def test_initial_tiles(self):
         bag = BagTiles()
-        self.assertEqual(len(bag.tiles),100)
+        self.assertEqual(len(bag.tiles),102)
