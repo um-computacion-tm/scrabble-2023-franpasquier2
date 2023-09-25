f0a83c180b451ed1c83998e8cf3510434c810e4f
Francisco Pasquier <franciscopasquier@pop-os.localdomain>
2023-09-24 21:29:33 -0300

    validation method and delete cell

--- test/test_cell.py
+++ test/test_cell.py
@@ -41,6 +41,25 @@ class TestCell(unittest.TestCase):
 
         self.assertEqual(cell.calculate_value(), 3)
 
+    def setUp(self):
+        self.cell = Cell()
+
+    def test_add_letter_valid(self):
+        letter = Tile(letter='A', value=1)
+        self.cell.add_letter(letter)
+        self.assertEqual(self.cell.letter, letter)
+
+    def test_add_letter_invalid(self):
+        with self.assertRaises(ValueError):
+            self.cell.add_letter('InvalidLetter')  # Intenta agregar un objeto que no es Tile
+        self.assertIsNone(self.cell.letter)  # La letra no debe haberse agregado
+
+    def test_remove_letter(self):
+        letter = Tile(letter='B', value=2)
+        self.cell.add_letter(letter)
+        self.cell.remove_letter()
+        self.assertIsNone(self.cell.letter)
+
 if __name__ == '__main__':
     unittest.main()
 
